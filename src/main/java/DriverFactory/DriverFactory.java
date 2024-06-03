43b84410315878d60f1bb7c9fffdf73017bfde80
Mahmoud Emad <Mahmoud.Emad@visionhealth.co.uk>
2024-06-03 14:49:52 +0300

    third commit

--- src/main/java/DriverFactory/DriverFactory.java
+++ src/main/java/DriverFactory/DriverFactory.java
@@ -1,12 +1,10 @@
 package DriverFactory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.Select;
+
 
 public class DriverFactory {
 
